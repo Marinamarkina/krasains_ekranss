dc1b8fcd8e0f678a342902092d03b16813fd69d4
Marinamarkina <113850073+Marinamarkina@users.noreply.github.com>
2022-09-27 21:58:58 +0300

    Add files via upload

---
+++ Viktorina/Viktorina/Form3.Designer.cs
@@ -0,0 +1,81 @@
+﻿
+namespace Viktorina
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            this.labRezultats = new System.Windows.Forms.Label();
+            this.labVirsraksts = new System.Windows.Forms.Label();
+            this.labKoment = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labRezultats
+            // 
+            this.labRezultats.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.labRezultats, "labRezultats");
+            this.labRezultats.ForeColor = System.Drawing.Color.Maroon;
+            this.labRezultats.Name = "labRezultats";
+            // 
+            // labVirsraksts
+            // 
+            resources.ApplyResources(this.labVirsraksts, "labVirsraksts");
+            this.labVirsraksts.ForeColor = System.Drawing.Color.Maroon;
+            this.labVirsraksts.Name = "labVirsraksts";
+            // 
+            // labKoment
+            // 
+            resources.ApplyResources(this.labKoment, "labKoment");
+            this.labKoment.ForeColor = System.Drawing.Color.Maroon;
+            this.labKoment.Name = "labKoment";
+            // 
+            // Form3
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Viktorina.Properties.Resources.Latvija;
+            this.Controls.Add(this.labKoment);
+            this.Controls.Add(this.labRezultats);
+            this.Controls.Add(this.labVirsraksts);
+            this.DoubleBuffered = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labRezultats;
+        private System.Windows.Forms.Label labVirsraksts;
+        private System.Windows.Forms.Label labKoment;
+    }
+}
---
+++ Viktorina/Viktorina/Form3.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Viktorina
+{
+    public partial class Form3 : Form
+    {
+        
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {  
+            if (Form2.punkti == 1) labRezultats.Text = Form2.punkti + " punkts";
+            else labRezultats.Text = Form2.punkti + " punkti";
+
+            switch (Form2.punkti)
+            {
+                case 10:
+                    labKoment.Text = "\nTu esi īsts savas zemes patriots! Turklāt vēl ar lieliskām zināšanām. Apsveicam! Tu atbildēji pareizi uz visiem jautājumiem!";
+                    break;
+                case 9:
+                    labKoment.Text = "\nAtbildes liecina, ka pazīsti Latviju, bet tev vēl ir fakti ko atklāt!";
+                    break;
+                case 8:
+                    labKoment.Text = "\nAtbildes liecina, ka pazīsti Latviju, bet tev vēl ir fakti ko atklāt!";
+                    break;
+                case 7:
+                    labKoment.Text = "\nAtbildes liecina, ka pazīsti Latviju, bet tev vēl ir fakti ko atklāt!";
+                    break;
+                case 6:
+                    labKoment.Text = "\nAtbildes liecina, ka pazīsti Latviju, bet tev vēl ir fakti ko atklāt!";
+                    break;
+                default:
+                    labKoment.Text = "Spriežot pēc atbildēm, zināšanas par Latviju tev ir jāpapildina! Diemžēl liela daļa tavu atbilžu bija nepareizas.";
+                    break;
+            }
+          
+        }
+    }
+}
